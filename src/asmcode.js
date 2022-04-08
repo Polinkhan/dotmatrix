b419c214d43cdd72caa7bb0f30d2725f9e0da8b9
Polin Khan <55098399+Polinkhan@users.noreply.github.com>
2022-04-08 16:22:53 +0600

    Update asmcode.js

--- src/asmcode.js
+++ src/asmcode.js
@@ -3,9 +3,9 @@ let print = document.querySelector("#code");
 Genarate = () => {
   console.log(isVisit);
   print.innerText = `=====================================\nA SEGMENT PARA PUBLIC 'CODE'\nASSUME CS: A\nORG 1000H\n\nS:\nMOV AL, 80H\nOUT 1EH, AL\n\nB:\nMOV AL, 00H\nOUT 1CH, AL\nMOV AL, 0FFH\nOUT 18H, AL\nOUT 1BH, AL\n\nC:\n`;
-  let row = [1, 1, 1, 1, 1, 1, 1, 1];
   for (let i = 0; i < 8; i++) {
     if (hasrow[i]) {
+      let row = [1, 1, 1, 1, 1, 1, 1, 1];
       row[i] = 0;
       let portB = parseInt(row.join(""), 2).toString(16);
       let portC = parseInt(isVisit[i].join(""), 2).toString(16);
